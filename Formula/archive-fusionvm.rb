743e8cdf8cae83911ddd27fb42ec5a59161735f5
Nick Reilingh <nreilingh@gmail.com>
2019-12-28 01:18:27 -0500

    added archive-fusionvm formula

---
+++ Formula/archive-fusionvm.rb
@@ -0,0 +1,10 @@
+class ArchiveFusionVM < Formula
+  desc "Simple shell script to suspend VMs in a directory and archive them to disk image."
+  homepage "https://github.com/NReilingh/archive-fusionvm/"
+  url "https://github.com/NReilingh/archive-fusionvm/releases/download/v0.0.1-alpha3/archive-fusionvm.tar.gz"
+  sha256 "9249e55196902d2cd81659f5068a0afeb64645f4f405a5c5e104c5cdeebd3e46"
+
+  def install
+    bin.install "archive_vms.zsh"
+  end
+end

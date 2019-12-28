e39127138723d1b1d13f0a4167044c7452fdaa08
Nick Reilingh <nreilingh@gmail.com>
2019-12-28 01:21:24 -0500

    Astonishingly good error messages, homebrew

--- Formula/archive-fusionvm.rb
+++ Formula/archive-fusionvm.rb
@@ -1,4 +1,4 @@
-class ArchiveFusionVM < Formula
+class ArchiveFusionvm < Formula
   desc "Simple shell script to suspend VMs in a directory and archive them to disk image."
   homepage "https://github.com/NReilingh/archive-fusionvm/"
   url "https://github.com/NReilingh/archive-fusionvm/releases/download/v0.0.1-alpha3/archive-fusionvm.tar.gz"

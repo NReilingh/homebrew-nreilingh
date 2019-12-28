9e0948e47c99aead42cf6fda3332188084ba5888
Nick Reilingh <nreilingh@gmail.com>
2019-12-28 02:23:33 -0500

    need to break this temporarily

--- Formula/archive-fusionvm.rb
+++ Formula/archive-fusionvm.rb
@@ -1,7 +1,7 @@
 class ArchiveFusionvm < Formula
   desc "Simple shell script to suspend VMs in a directory and archive them to disk image."
   homepage "https://github.com/NReilingh/archive-fusionvm/"
-  url "https://github.com/NReilingh/archive-fusionvm/releases/download/v0.0.1-alpha3/archive-fusionvm.tar.gz"
+  url "https://github.com/NReilingh/archive-fusionvm/releases/download/v0.0.1-alpha3/archive-fusionvm-v0.0.1-alpha3.tar.gz"
   sha256 "9249e55196902d2cd81659f5068a0afeb64645f4f405a5c5e104c5cdeebd3e46"
 
   def install

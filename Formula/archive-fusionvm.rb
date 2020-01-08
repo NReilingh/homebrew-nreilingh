cc0799bb705cfa7a749fef07b339a4329503d157
Nick Reilingh <nreiling@bard.edu>
2020-01-08 16:58:27 -0500

    archive-fusionvm 0.1.1
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/archive-fusionvm.rb
+++ Formula/archive-fusionvm.rb
@@ -1,8 +1,8 @@
 class ArchiveFusionvm < Formula
   desc "Simple shell script to suspend VMs in a directory and archive them to disk image."
   homepage "https://github.com/NReilingh/archive-fusionvm/"
-  url "https://github.com/NReilingh/archive-fusionvm/releases/download/v0.1.0/archive-fusionvm-v0.1.0.tar.gz"
-  sha256 "18175de8aab0a3b2345baa20045727718a09cb1e3ff85dff4c35e8035b138d48"
+  url "https://github.com/NReilingh/archive-fusionvm/releases/download/v0.1.1/archive-fusionvm-v0.1.1.tar.gz"
+  sha256 "d4e30c6845b309c2e50fcbea1fc905bc2d89a92899362226706211e20a7533e0"
 
   def install
     bin.install "archive_fusionvm"

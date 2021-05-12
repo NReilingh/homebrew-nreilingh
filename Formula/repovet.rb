9a2e4c515b7734b828cb331422063f053adf33f2
Nick Reilingh <nreiling@bard.edu>
2021-05-12 18:04:29 -0400

    repovet 0.0.6
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/repovet.rb
+++ Formula/repovet.rb
@@ -1,8 +1,8 @@
 class Repovet < Formula
   desc "Validation tool for repo.cue files"
   homepage "https://github.com/NReilingh/describe-repo-schema/"
-  url "https://github.com/NReilingh/describe-repo-schema/releases/download/v0.0.5/repovet-v0.0.5.tar.gz"
-  sha256 "55192149cc9437a4c7b327eacd3df8f995d5e617f6d03157a93084f0381e3e0f"
+  url "https://github.com/NReilingh/describe-repo-schema/releases/download/v0.0.6/repovet-v0.0.6.tar.gz"
+  sha256 "7ba4237fb3b0a6631cffa56e0f0f18d2b2dca0c36224c2d4e96708ab0920af25"
   depends_on "cuelang/tap/cue"
 
   def install

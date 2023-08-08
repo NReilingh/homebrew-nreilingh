8b42080225616cc6b2b60958dd45f58fc46247ac
Nick Reilingh <nreilingh@gmail.com>
2023-08-08 19:17:45 -0400

    fix repovet dependency

--- Formula/repovet.rb
+++ Formula/repovet.rb
@@ -3,7 +3,7 @@ class Repovet < Formula
   homepage "https://github.com/NReilingh/describe-repo-schema/"
   url "https://github.com/NReilingh/describe-repo-schema/releases/download/v0.0.6/repovet-v0.0.6.tar.gz"
   sha256 "7ba4237fb3b0a6631cffa56e0f0f18d2b2dca0c36224c2d4e96708ab0920af25"
-  depends_on "cuelang/tap/cue"
+  depends_on "cue"
 
   def install
     bin.install "repovet"
